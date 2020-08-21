5f95bf5a00cefd84eb56a5b08c337ba983066519
Miller Rich <william.miller.rich@gmail.com>
2020-08-21 15:44:22 -0500

    fix for loop and formatting

--- script.js
+++ script.js
@@ -3,38 +3,39 @@ var generateBtn = document.querySelector("#generate");
 //capital letters
 var allCap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var cap = allCap.split("");
-  console.log(cap);
+console.log(cap);
 //lower case letters
 var allLow = "abcdefghijklmnopqrstuvwxyz";
 var low = allLow.split("");
-  console.log(low);
+console.log(low);
 //special characters
 var allSpec = "!@#$%^&*()-=_+;:[{]}";
 var spec = allSpec.split("");
-  console.log(spec);
+console.log(spec);
 //numbers
 var allNumbers = "0123456789"
 var num = allNumbers.split("");
-  console.log(num);
-
+console.log(num);
+var validChar = [];
+var passLength = 0;
 
 // Write password to the #password input
 function writePassword() {
-  
+
   // runs function passParam when generate password button is clicked
   passParam();
-   var password = generatePassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   passwordText.value = password;
-  
+
 }
 
 // function has user input number >= 8 or <= 128
 // runs function charParam once parameters are met
 function passParam() {
-  
-  var passLength = parseInt(prompt("How many characters would you like in your password? Choose any number from 8 to 128."));
+
+  passLength = parseInt(prompt("How many characters would you like in your password? Choose any number from 8 to 128."));
   console.log("password length: " + passLength);
   if (isNaN(passLength) || passLength === "") {
     alert("Please enter a valid number 8 - 128");
@@ -45,36 +46,41 @@ function passParam() {
   } else {
     charParam();
   }
-}  
-
-function charParam () {
-  var validChar = [];
+}
+//function has user confirm which characters to include
+function charParam() {
   var lowerCase = confirm("Include lowercase letters?");
-    if (lowerCase) {
-      validChar = validChar.concat(low);
-    }
+  if (lowerCase) {
+    validChar = validChar.concat(low);
+  }
   var upperCase = confirm("Include upper case letters?");
-    if (upperCase) {
-      validChar = validChar.concat(cap);
-    }
+  if (upperCase) {
+    validChar = validChar.concat(cap);
+  }
   var specChar = confirm("Include special characters?");
-    if (specChar) {
-      validChar = validChar.concat(spec);
-    }
+  if (specChar) {
+    validChar = validChar.concat(spec);
+  }
   var numbers = confirm("Include numbers?");
-    if (numbers) {
-      validChar = validChar.concat(num);
-    }
-    
-    //array including all characters which password is to be generated from
-    console.log(validChar);
+  if (numbers) {
+    validChar = validChar.concat(num);
+  }
+
+  //array including all characters which password is to be generated from
+  console.log(validChar);
 
 }
 
 function generatePassword() {
-   var password = validChar[Math.floor(Math.random() * validChar.length)];
-  console.log(password);
-} 
+  var passFin = "";
+  for (var i = 0; i < passLength; i++) {
+    var random = validChar[Math.floor(Math.random() * validChar.length)];
+    console.log(random);
+    passFin += random;
+  }
+  return passFin;
+}
+
 
 
 
@@ -82,5 +88,5 @@ function generatePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); {
-  
+
 }
